242199a3f97435f5f5d949a2a91f557904982087
Denis <da962525089@gmail.com>
2023-04-03 22:06:14 -0500

    Creando Proyecto Conversor de moneda Alura

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.currencymoneyalura {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.currencymoneyalura to javafx.fxml;
+    exports com.currencymoneyalura;
+}
